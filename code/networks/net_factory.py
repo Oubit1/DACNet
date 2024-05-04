d191d8fe22f6d3869fa843ac16b5da86cd562b82
Oubit1 <114571962+Oubit1@users.noreply.github.com>
2024-05-04 16:34:38 +0800

    Update net_factory.py

--- code/networks/net_factory.py
+++ code/networks/net_factory.py
@@ -1,5 +1,5 @@
-from networks.VNet import VNet,ACNet3d_V2
-from networks.unet import UNet,ACNet2d_v3，ACNet2d_v2
+from networks.VNet import VNet,DACNet3d_V2
+from networks.unet import UNet,DACNet2d_v3，DACNet2d_v2
 def net_factory(net_type="unet", in_chns=1, class_num=4, mode = "train"):
     if net_type == "vnet" and mode == "train":
         net = VNet(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
@@ -7,14 +7,14 @@ def net_factory(net_type="unet", in_chns=1, class_num=4, mode = "train"):
         net = VNet(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
     elif net_type == "unet":
         net = UNet(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "acnet2d_v3":
-        net = ACNet2d_v3(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "acnet2d_v2":
-        net = ACNet2d_v2(in_chns=in_chns, class_num=class_num).cuda()
-    elif net_type == "acnet3d_v2" and mode == "train":
-        net = ACNet3d_V2(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
-    elif net_type == "acnet3d_v2" and mode == "test":
-        net = ACNet3d_V2(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=False).cuda()
+    elif net_type == "dacnet2d_v3":
+        net = DACNet2d_v3(in_chns=in_chns, class_num=class_num).cuda()
+    elif net_type == "dacnet2d_v2":
+        net = DACNet2d_v2(in_chns=in_chns, class_num=class_num).cuda()
+    elif net_type == "dacnet3d_v2" and mode == "train":
+        net = DACNet3d_V2(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
+    elif net_type == "dacnet3d_v2" and mode == "test":
+        net = DACNet3d_V2(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=False).cuda()
     elif net_type == "vnet" and mode == "test":
         net = VNet(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=False).cuda()
     return net
